34d161327c0bd9bfef80a2b6394cf5c97ffdadec
commi235 <moritz.klein@it-twins.de>
2017-11-10 00:34:11 +0100

    minor chnages and first typescript try

--- src/js/mk_apex_jstree.ts
+++ src/js/mk_apex_jstree.ts
@@ -1,4 +1,3 @@
-//@ts-check
 /// <reference types="orclapex-js" />
 /// <reference types="jquery" />
 (
@@ -6,12 +5,11 @@
   {
     "use strict";
 
-    window.mkApexJsTreeInit = function( itemId, ajaxIdent ) {
-      var out   = util.htmlBuilder()
-        , item$ = $("#" + itemId)
-        , jsTreeItem
-        , selectedValues
-        , jsTree;
+    window.mkApexJsTreeInit = function( itemId: string, ajaxIdent: string ) {
+      var out: apex.util.htmlBuilderPrototype = util.htmlBuilder()
+        , item$: JQuery<HTMLElement> = $("#" + itemId)
+        , jsTreeItem: JQuery<HTMLElement>
+        , jsTree: JQuery<HTMLElement>;
 
       out.markup( "<div " )
          .attr( "id", "#" + itemId + "_jstree" )
@@ -20,7 +18,7 @@
       item$.parent().append(jsTreeItem);
       jsTree = jsTreeItem.jstree( { "core" : { "data" : { "url" : apex.server.pluginUrl(ajaxIdent, { x01: item$.val() }) } }, "plugins" : [ "checkbox" ]} );
       jsTree.on("changed.jstree", function( e, data ) {
-        item$.val(data.selected.join(":")).change();
+        item$.val(data.selected.join(":"));
       });
     }
   }
